67f26378903bcd8e3c124c69ce44a7dc9a9c3d45
Alexander Furmanchuk <afurmanchuk@neoflex.ru>
2025-05-07 13:08:39 +0300

    fix

--- xela_module/hooks/post_gen.dart
+++ xela_module/hooks/post_gen.dart
@@ -3,13 +3,20 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 void run(HookContext context) async {
-   await _runBuild(context);
+  await _runBuild(context);
 }
 
 Future<void> _runBuild(HookContext context) async {
   final progress = context.logger.progress('Build');
 
-  final List<String> commands = ['exec', '--scope', '${context.vars['moduleName'].toString().toLowerCase()}', ' -- "dart run build_runner build -d"'];
+  final List<String> commands = [
+    'exec',
+    '--scope',
+    '${context.vars['moduleName'].toString().toLowerCase()}',
+    ' -- "dart run build_runner build -d"'
+  ];
+
+  context.logger.info(commands.join(' '));
 
   await Process.run('melos', commands);
 
