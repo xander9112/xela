f5af95a18d304139ba616781eee7bb3bc33c14a3
Alexander Furmanchuk <afurmanchuk@neoflex.ru>
2025-05-07 13:02:31 +0300

    fix

--- xela_module/hooks/post_gen.dart
+++ xela_module/hooks/post_gen.dart
@@ -9,16 +9,9 @@ void run(HookContext context) async {
 Future<void> _runBuild(HookContext context) async {
   final progress = context.logger.progress('Build');
 
-  final List<String> commands = ['exec --scope ${context.vars['moduleName'].toString().toLowerCase()} -- "dart run build_runner build -d"'];
+  final List<String> commands = ['exec', '--scope', '${context.vars['moduleName'].toString().toLowerCase()}', ' -- "dart run build_runner build -d"'];
 
-  await Process.run('melos', commands).onError(
-    (error, stackTrace) {
-      return Process.run(
-        'melos',
-        [...commands],
-      );
-    },
-  );
+  await Process.run('melos', commands);
 
   progress.complete();
 }
